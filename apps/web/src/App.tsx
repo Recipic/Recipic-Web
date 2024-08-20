f9fefa33c657980ca303830d37693efaf5060358
minseong0324 <kwan0324@naver.com>
2024-08-21 07:00:14 +0900

    Feat: ErrorBoundary component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
-import { Suspense } from 'react';
 import useScrollToTop from './hooks/useScrollToTop';
 
 const App = () => {
   useScrollToTop();
 
   return (
-    <Suspense>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </Suspense>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 };
 
