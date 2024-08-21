053ba80bafde0fa4cba7ccd374bc7436a62c0caf
minseong0324 <kwan0324@naver.com>
2024-08-21 21:49:33 +0900

    Feat: title for each section

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { PageLayout } from '@recipic-packages/ui';
 import { CarouselWithBanners } from '../components/CarouselWithBanners';
+import { SectionTitle } from '../components/SectionTitle';
 
 export default function Home() {
   return (
     <PageLayout isTabBarVisible>
       <CarouselWithBanners />
+      <SectionTitle title="이번 달 인기 레시피" />
+      <SectionTitle title="최신 HOT 브랜드" />
+      <SectionTitle title="유명인의 인기 레시피" />
       <></>
     </PageLayout>
   );
