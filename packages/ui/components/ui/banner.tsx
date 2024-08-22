dbe2047a7e2aac053696614d8bd8ece326a57e2a
minseong0324 <kwan0324@naver.com>
2024-08-22 15:23:45 +0900

    Design: fix carousel style

--- packages/ui/components/ui/banner.tsx
+++ packages/ui/components/ui/banner.tsx
@@ -8,7 +8,7 @@ type BannerProps = {
 
 export function Banner({ imageUrl, onClick }: BannerProps) {
   return (
-    <div className="px-4 mx-auto max-w-screen-lg w-full">
+    <div className="w-full">
       <Button variant="ghost" className="p-0 w-full h-auto aspect-[16/9]" onClick={onClick}>
         <img src={imageUrl} alt="Banner" className="w-full h-full object-cover rounded-lg" />
       </Button>
