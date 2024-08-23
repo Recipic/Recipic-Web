be9a844037620b8ee1540bb47ac72024d4bc1efd
minseong0324 <kwan0324@naver.com>
2024-08-23 14:26:32 +0900

    Fix: PageLayout scroll bug fix

--- apps/web/src/components/CarouselWithRecipeDetailImage/index.tsx
+++ apps/web/src/components/CarouselWithRecipeDetailImage/index.tsx
@@ -7,7 +7,7 @@ type TCarouselWithRecipeDetailImageProps = {
 
 export function CarouselWithRecipeDetailImage({ detailImages }: TCarouselWithRecipeDetailImageProps) {
   return (
-    <Carousel autoScroll showDots autoScrollInterval={5000}>
+    <Carousel autoScroll autoScrollInterval={5000}>
       {detailImages.map((image, index) => (
         <div key={index} className="flex-[1_0_100%] h-[350px] overflow-hidden">
           <img src={image} alt={`Detail ${index}`} className="w-full h-full object-cover" />
