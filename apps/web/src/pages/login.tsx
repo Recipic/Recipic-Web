c9b62cdd7143b9383bed648ca600306aa4fe2915
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-24 19:35:38 +0900

    FEAT: Implement logic for receiving and sending authorization code to server

--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -2,8 +2,7 @@
 import React, { useEffect } from 'react';
 import { PageLayout } from '@recipic-packages/ui';
 import RecipicIcon from '../../public/Icons/RecipicIcon.png'; // 경로 확인 필요
-import kakaoLoginButton from '../assets/images/kakao_login_medium_wide.png'; // 카카오 로그인 버튼 이미지 경로
-import { REST_API_KEY, REDIRECT_URL, BASE_URL } from '../env';
+import { BASE_URL } from '../env';
 import axios from 'axios';
 const Login: React.FC = () => {
   //   카카오 정보제공동의 후 인가코드(Authorization code) 획득
@@ -12,28 +11,32 @@ const Login: React.FC = () => {
     const code = queryParams.get('code');
     if (code) {
       console.log('Authorization code: ', code);
-      //sendAuthorizationCode(code);
+      //   서버에 인가코드 전송
+      sendCodeToServer(code);
     } else {
       console.log('Authorization code를 받지 못했습니다.');
     }
   }, []);
 
+  // response data 정의(후에 수정?)
   interface ApiResponse {
     message: string;
+    accessToken: string;
   }
 
   const sendCodeToServer = async (code: string): Promise<void> => {
     try {
       const response = await axios.post<ApiResponse>(
-        BASE_URL,
+        `${BASE_URL}/api/auth/kakao`,
         {
-          code,
+          authorizationCode: code,
         },
         {
           headers: { 'Content-Type': 'application/json' },
         },
       );
       console.log('Server response:', response.data);
+      localStorage.setItem('accessToken: ', response.data.accessToken);
     } catch (error) {
       console.error('Error:', error);
     }
@@ -44,7 +47,7 @@ const Login: React.FC = () => {
         <img src={RecipicIcon} alt="Recipic Logo" className="w-24 mb-4" />
         <h1 className="text-xl font-bold mb-6">RECIPICK</h1>
 
-        <button className="text-gray-300 font-xs">로그인중입니다</button>
+        <button className="text-gray-300 font-xs">로그인중입니다...</button>
       </div>
     </PageLayout>
   );
