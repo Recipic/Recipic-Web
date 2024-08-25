3d043cc2a9ee96cd19efea5c80281cf9922a61aa
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-25 12:18:17 +0900

    FIX: change Header, TopNavBar props & modify padding

--- apps/web/src/pages/mycomments.tsx
+++ apps/web/src/pages/mycomments.tsx
@@ -13,46 +13,11 @@ export interface CommentProps {
 }
 
 export default function MyComments() {
-  const commentItems: CommentProps[] = [
-    {
-      mainTitle: '요아정 꿀조합!!! 딸기바나나',
-      brandImage: <ImageIcon />,
-      comment: '너무 꿀조합이네요 우와~~~',
-      likesCount: 5,
-      publishDate: '2024-08-22',
-      recipieId: 1,
-    },
-    {
-      mainTitle: '서브웨이는 역시 BMT지~~~',
-      brandImage: <ImageIcon />,
-      comment: '인정하는 바입니다. 소스는 무조건 랜치와 스위트칠리로...',
-      likesCount: 4,
-      publishDate: '2024-08-21',
-      recipieId: 2,
-    },
-    {
-      mainTitle: '요아정 꿀조합!!! 딸기바나나',
-      brandImage: <ImageIcon />,
-      comment: '역시 이 조합이 최고네요. 매번 먹어도 질리지가 않아요!',
-      likesCount: 3,
-      publishDate: '2024-08-20',
-      recipieId: 3,
-    },
-    {
-      mainTitle: '문의하기',
-      brandImage: <ImageIcon />,
-      comment: '앱 사용 중 문의사항이 있습니다.',
-      likesCount: 2,
-      publishDate: '2024-08-19',
-      recipieId: 4,
-    },
-  ];
-
   return (
     <PageLayout>
-      <TopNavBar />
-      <Header title="내가 작성한 댓글"></Header>
-      <div className="flex flex-col items-center gap-y-6 p-6">
+      <Header title="내가 작성한 댓글" order="second" />
+      <TopNavBar order="first" />
+      <div className="flex flex-col items-center gap-y-6 p-6 mt-24">
         {commentItems.map(comment => (
           <MyCommentBox
             mainTitle={comment.mainTitle}
@@ -67,3 +32,47 @@ export default function MyComments() {
     </PageLayout>
   );
 }
+
+//목데이터. 추후 삭제 예정
+const commentItems: CommentProps[] = [
+  {
+    mainTitle: '요아정 꿀조합!!! 딸기바나나',
+    brandImage: <ImageIcon />,
+    comment: '너무 꿀조합이네요 우와~~~',
+    likesCount: 5,
+    publishDate: '2024-08-22',
+    recipieId: 1,
+  },
+  {
+    mainTitle: '서브웨이는 역시 BMT지~~~',
+    brandImage: <ImageIcon />,
+    comment: '인정하는 바입니다. 소스는 무조건 랜치와 스위트칠리로...',
+    likesCount: 4,
+    publishDate: '2024-08-21',
+    recipieId: 2,
+  },
+  {
+    mainTitle: '요아정 꿀조합!!! 딸기바나나',
+    brandImage: <ImageIcon />,
+    comment: '역시 이 조합이 최고네요. 매번 먹어도 질리지가 않아요!',
+    likesCount: 3,
+    publishDate: '2024-08-20',
+    recipieId: 3,
+  },
+  {
+    mainTitle: '문의하기',
+    brandImage: <ImageIcon />,
+    comment: '앱 사용 중 문의사항이 있습니다.',
+    likesCount: 2,
+    publishDate: '2024-08-19',
+    recipieId: 4,
+  },
+  {
+    mainTitle: '문의하기',
+    brandImage: <ImageIcon />,
+    comment: '앱 사용 중 문의사항이 있습니다.',
+    likesCount: 2,
+    publishDate: '2024-08-19',
+    recipieId: 4,
+  },
+];
