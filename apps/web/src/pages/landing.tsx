2178b047066c3f55b86cb4c08478e6c224a0b9a6
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-25 20:32:06 +0900

    FIX: modify kakaoLoginButton css & modify login logic

--- apps/web/src/pages/landing.tsx
+++ apps/web/src/pages/landing.tsx
@@ -2,10 +2,9 @@
 import React, { useEffect } from 'react';
 import { PageLayout } from '@recipic-packages/ui';
 import RecipicIcon from '../../public/Icons/RecipicIcon.png'; // 경로 확인 필요
-import kakaoLoginButton from '../assets/images/kakao_login_medium_wide.png'; // 카카오 로그인 버튼 이미지 경로
+import kakaoLogo from '../assets/kakaoLogo.svg';
 
-import axios from 'axios';
-const Landing: React.FC = () => {
+export default function Landing() {
   const redirectUrl = import.meta.env.VITE_REDIRECT_URL;
   const apiKey = import.meta.env.VITE_REST_API_KEY;
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${apiKey}&redirect_uri=${redirectUrl}&response_type=code`;
@@ -20,12 +19,16 @@ const Landing: React.FC = () => {
         <img src={RecipicIcon} alt="Recipic Logo" className="w-24 mb-4" />
         <h1 className="text-xl font-bold mb-6">RECIPICK</h1>
         <div className="my-2">
-          <img src={kakaoLoginButton} alt="Kakao Login" className="cursor-pointer" onClick={loginHandler} />
+          <button
+            onClick={loginHandler}
+            className="bg-[#FEE500] text-black font-bold py-2 px-20 mx-4 rounded inline-flex justify-between items-center"
+          >
+            <img src={kakaoLogo} alt="Kakao Login" className="cursor-pointer w-4" onClick={loginHandler} />
+            <span className="pl-2">카카오 로그인하기</span>
+          </button>
         </div>
         <button className="text-gray-300 underline font-xs">카카오 계정으로 간편하게 로그인하세요!</button>
       </div>
     </PageLayout>
   );
-};
-
-export default Landing;
+}
