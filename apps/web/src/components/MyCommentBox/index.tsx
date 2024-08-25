c363c9b04f368af8f58e13f1617d17723f95f0ba
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-25 21:55:28 +0900

    FIX: unify function() instead of React.FC

--- apps/web/src/components/MyCommentBox/index.tsx
+++ apps/web/src/components/MyCommentBox/index.tsx
@@ -1,41 +1,33 @@
 import { HeartFilledIcon } from '@radix-ui/react-icons';
 import { CommentProps } from '../../pages/mycomments';
 import { useNavigate } from 'react-router-dom';
-const CommentComponent: React.FC<CommentProps> = ({
-  mainTitle,
-  brandImage,
-  comment,
-  likesCount,
-  publishDate,
-  recipieId,
-}) => {
+export default function CommentComponent(props: CommentProps) {
   const navigate = useNavigate();
+  const { mainTitle, brandImage, comment, likesCount, publishDate, recipieId } = props;
   const handleClick = () => {
     navigate(`/recipies/${recipieId}`);
   };
   return (
     <div
       onClick={handleClick}
-      className="w-[90%] flex flex-col border rounded-lg px-4 shadow-sm bg-white cursor-pointer transform transition-transform duration-300 ease-out hover:scale-105 hover:shadow-lg"
+      className="w-[90%] flex flex-col border rounded-lg p-4 shadow-sm bg-white cursor-pointer transform transition-transform duration-300 ease-out hover:scale-105 hover:shadow-lg"
     >
-      <div className="flex justify-between items-start mb-1 mt-2">
+      <div className="flex justify-between items-start mb-3">
         <div className="flex items-center">
-          <div className="w-8 h-8 rounded-full mr-4 bg-slate-200 flex justify-center items-center">{brandImage}</div>
+          <div className="w-10 h-10 rounded-full mr-4 bg-slate-200 flex justify-center items-center">{brandImage}</div>
           <h3 className="font-bold text-base">{mainTitle.length > 12 ? mainTitle.slice(0, 12) + '...' : mainTitle}</h3>
         </div>
         <button className="text-gray-400 hover:text-gray-600">X</button>
       </div>
       <hr className="mb-4" />
-      <p className="text-gray-700 mb-4">{comment.length > 30 ? comment.slice(0, 22) + '...' : comment}</p>
+      <p className="text-gray-700 mb-4 ">{comment.length > 30 ? comment.slice(0, 30) + '...' : comment}</p>
       <div className="flex justify-between items-center text-sm text-gray-500">
-        <span className="text-sm">{publishDate}</span>
-        <div className="flex items-center mb-2">
+        <span className="text-3">{publishDate}</span>
+        <div className="flex items-center">
           <HeartFilledIcon className="text-pink-500 mr-1" />
-          <span className="text-sm">{likesCount}</span>
+          <span className="text-3">{likesCount}</span>
         </div>
       </div>
     </div>
   );
-};
-
-export default CommentComponent;
+}
