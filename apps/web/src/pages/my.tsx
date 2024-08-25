e3f84383e10b82ee3159b3243aa286ef24412993
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-26 03:13:01 +0900

    FEAT: add userProfile

--- apps/web/src/pages/my.tsx
+++ apps/web/src/pages/my.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Header, PageLayout, Button } from '@recipic-packages/ui';
-import { GearIcon } from '@radix-ui/react-icons';
+import BannerComponent from '../components/MyBannerComponent';
+import DislikeIcon from '../assets/images/DislikeIcon.png';
 import { useNavigate } from 'react-router-dom';
+import {
+  GearIcon,
+  Pencil2Icon,
+  KeyboardIcon,
+  BellIcon,
+  PaperPlaneIcon,
+  InstagramLogoIcon,
+  FileTextIcon,
+} from '@radix-ui/react-icons';
 
 export default function My() {
   const navigate = useNavigate();
@@ -18,7 +28,23 @@ export default function My() {
           <GearIcon className="h-7 w-7" />
         </Button>
       </Header>
-      {/* 페이지 내용 */}
+      <br />
+      <div className="flex items-center m-6">
+        <img src="https://via.placeholder.com/100" alt="Profile" className="w-12 h-12 rounded-full" />
+        <div className="ml-4">
+          <div className="flex items-center">
+            <span className="text-xl font-semibold">먹부림 사냥꾼</span>
+            <Pencil2Icon className="ml-2 w-5 h-5" />
+          </div>
+        </div>
+      </div>
+      <div className="my-12"></div>
     </PageLayout>
   );
+}
+
+interface BannerProps {
+  title: string;
+  icon: React.ReactNode;
+  route: string;
 }
