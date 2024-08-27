cc67823fdced5de216da79f1eb33e85eb8101c17
minseong <kwan0324@naver.com>
2024-08-27 15:24:16 +0900

    Style: console.log remove

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -16,7 +16,7 @@ import { useGetRecipeRankList } from '@/hooks/useGetRecipeRankList';
 export default function Home() {
   const navigate = useNavigate();
   const { recipeRankListData } = useGetRecipeRankList();
-  console.log(recipeRankListData);
+  //console.log(recipeRankListData);
   /**
    * 브랜드 버튼 클릭을 처리하는 함수
    * @param {TBrand} searchBrand - 클릭된 브랜드
