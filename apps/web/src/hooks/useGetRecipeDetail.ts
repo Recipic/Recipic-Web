d943e140995d98628588d060b142ab077d2b0d12
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-08-28 15:57:09 +0900

    FEAT: add recipeDetail hook useage and loggin to recipeDetail.tsx

---
+++ apps/web/src/hooks/useGetRecipeDetail.ts
@@ -0,0 +1,18 @@
+/*나중에 recipeId: number로 바꿔야하나?*/
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { TGetRecipeDetailResponse } from '@/apis/recipeDetail/type';
+import { getRecipeDetailQueryKey } from '@/constants/queryKeys';
+
+export const useGetRecipeDetail = (recipeId: string) => {
+  const {
+    data: recipeRankListData,
+    isLoading,
+    error,
+  } = useSuspenseQuery<TGetRecipeDetailResponse>({
+    queryKey: getRecipeDetailQueryKey(recipeId).queryKey,
+    queryFn: getRecipeDetailQueryKey(recipeId).queryFn,
+    staleTime: 1000 * 60 * 60, // 1시간
+    gcTime: 1000 * 60 * 40, // 40분
+  });
+  return { recipeRankListData, isLoading, error };
+};
