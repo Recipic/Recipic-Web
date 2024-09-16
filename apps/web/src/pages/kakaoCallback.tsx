d95704acf5b694b8fc756f7747fee4b4ce19d602
minseong <kwan0324@naver.com>
2024-09-17 04:32:37 +0900

    Fix: change useParams to useSearchParams

--- apps/web/src/pages/kakaoCallback.tsx
+++ apps/web/src/pages/kakaoCallback.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import FallbackUI from '@/components/common/FallbackUI';
 import { usePostAuthorizationCode } from '@/hooks/usePostAuthorizationCode';
 
 export default function KakaoCallback() {
-  const params = useParams<{ code: string }>();
-  const authorizationCode = params.code;
+  const [searchParams] = useSearchParams();
+  const authorizationCode: string | null = searchParams.get('code');
 
   const { mutate: mutateAuthorizationCode } = usePostAuthorizationCode();
 
   useEffect(() => {
-    if (authorizationCode) {
+    if (authorizationCode !== null) {
       mutateAuthorizationCode({ authorizationCode: authorizationCode });
     }
   }, [authorizationCode, mutateAuthorizationCode]);
