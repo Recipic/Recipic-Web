91d29d20bfbb00ac66c1ddecb79057c98a69f578
minseong <kwan0324@naver.com>
2024-09-23 16:45:01 +0900

    Fix: fix type, props at BottomFixedButtonWithGradientDiv

--- apps/web/src/components/common/Buttons/BottomFixedButtonWithGradientDiv/index.tsx
+++ apps/web/src/components/common/Buttons/BottomFixedButtonWithGradientDiv/index.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Button } from '@recipic-packages/ui';
+import { Button, ButtonProps } from '@recipic-packages/ui';
 
-type TBottomFixedButtonProps = {
-  type?: 'submit' | 'button';
-  form?: string;
+type TBottomFixedButtonProps = Omit<ButtonProps, 'className'> & {
   buttonText: string;
-  onClick: () => void;
 };
 
-export function BottomFixedButtonWithGradientDiv({
-  buttonText,
-  onClick,
-  type = 'button',
-  form,
-}: TBottomFixedButtonProps) {
+export function BottomFixedButtonWithGradientDiv({ buttonText, ...props }: TBottomFixedButtonProps) {
   return (
     <div className="fixed bottom-0 left-0 right-0 max-w-lg mx-auto">
       <div className="h-8 bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.7)] to-white"></div>
       <div className="px-4 pb-4 bg-white">
-        <Button type={type} form={form} className="w-full h-12" onClick={onClick}>
+        <Button className="w-full h-12" {...props}>
           {buttonText}
         </Button>
       </div>
