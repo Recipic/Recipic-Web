0bc7df4e191f3e3787f7eb4a821ff1654760ce24
minseong <kwan0324@naver.com>
2024-09-26 21:19:58 +0900

    Fix: modify interceptor

--- apps/web/src/apis/axios.ts
+++ apps/web/src/apis/axios.ts
@@ -20,58 +20,54 @@ export const instance: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
-export const useAxiosWithReissue = () => {
-  instance.interceptors.request.use(
-    (config: CustomAxiosRequestConfig): CustomAxiosRequestConfig => {
-      const accessToken = localStorage.getItem('accessToken');
-      if (accessToken && config.headers) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      }
-      return config;
-    },
-    (error: AxiosError): Promise<AxiosError> => {
-      return Promise.reject(error);
-    },
-  );
+instance.interceptors.request.use(
+  (config: CustomAxiosRequestConfig): CustomAxiosRequestConfig => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken && config.headers) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  },
+);
 
-  instance.interceptors.response.use(
-    (response: AxiosResponse): AxiosResponse => response,
-    async (error: AxiosError): Promise<AxiosResponse> => {
-      const originalRequest = error.config as CustomAxiosRequestConfig;
-      console.error('Response Error:', error); // 디버깅용
-      console.log('Error Response:', error.response); // 디버깅용
-      console.log('Error Config:', error.config); // 디버깅용
-      if (error.response?.status !== 401 || originalRequest._retry) {
+instance.interceptors.response.use(
+  (response: AxiosResponse): AxiosResponse => response,
+  async (error: AxiosError): Promise<AxiosResponse> => {
+    const originalRequest = error.config as CustomAxiosRequestConfig;
+    console.error('Response Error:', error); // 디버깅용
+    console.log('Error Response:', error.response); // 디버깅용
+    console.log('Error Config:', error.config); // 디버깅용
+    if (error.response?.status !== 401 || originalRequest._retry) {
+      return Promise.reject(error);
+    }
+
+    originalRequest._retry = true;
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    try {
+      const response: AxiosResponse<TAuthResponse, TReissueRequestData> = await instance.post('/auth/reissue', {
+        accessToken: accessToken,
+      });
+
+      if (response.status !== 200) {
         return Promise.reject(error);
       }
 
-      originalRequest._retry = true;
+      const newAccessToken = response.data.accessToken;
 
-      const accessToken = localStorage.getItem('accessToken');
+      localStorage.setItem('accessToken', newAccessToken);
 
-      try {
-        const response: AxiosResponse<TAuthResponse, TReissueRequestData> = await instance.post('/auth/reissue', {
-          accessToken: accessToken,
-        });
-
-        if (response.status !== 200) {
-          return Promise.reject(error);
-        }
-
-        const newAccessToken = response.data.accessToken;
-
-        localStorage.setItem('accessToken', newAccessToken);
-
-        if (originalRequest.headers) {
-          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        }
-
-        return instance(originalRequest);
-      } catch (refreshError) {
-        return Promise.reject(refreshError);
+      if (originalRequest.headers) {
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
       }
-    },
-  );
 
-  return instance;
-};
+      return instance(originalRequest);
+    } catch (refreshError) {
+      return Promise.reject(refreshError);
+    }
+  },
+);
