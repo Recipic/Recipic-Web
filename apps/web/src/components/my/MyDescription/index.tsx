e939028256930aa900858926ae80cb656d25c01e
minseong <kwan0324@naver.com>
2024-10-02 17:17:23 +0900

    Design: add border radius

--- apps/web/src/components/my/MyDescription/index.tsx
+++ apps/web/src/components/my/MyDescription/index.tsx
@@ -6,7 +6,7 @@ type TMyDescriptionProps = {
 
 export function MyDescription({ description }: TMyDescriptionProps) {
   return (
-    <div className="bg-gray-100 p-4 my-4">
+    <div className="bg-gray-100 p-4 my-4 rounded-lg">
       <p className="text-regular14 text-gray-500 whitespace-pre-line">
         {description !== null ? description : '아직 자기소개가 없어요\n자기소개를 작성하고 프로필을 꾸며봐요'}
       </p>
