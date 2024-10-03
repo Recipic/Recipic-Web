fc2518d5e4f6a43e1fb75b25538b60f758f7abcd
minseong <kwan0324@naver.com>
2024-10-03 20:22:32 +0900

    Fix: Prevent api calls when you are not logged in

--- apps/web/src/hooks/useGetNotificationList.ts
+++ apps/web/src/hooks/useGetNotificationList.ts
@@ -1,16 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 import { TGetNotificationListResponse } from '@/apis/notification/type';
 import { getNotificationListQueryKey } from '@/constants/queryKeys';
 import { toast } from 'sonner';
+import { useAuth } from '@/contexts/authContext';
 
 export const useGetNotificationList = () => {
+  const { isLoggedIn } = useAuth();
+
   const {
     data: notificationListData,
     isLoading,
     error,
   } = useQuery<TGetNotificationListResponse>({
     queryKey: getNotificationListQueryKey().queryKey,
-    queryFn: getNotificationListQueryKey().queryFn,
+    queryFn: isLoggedIn ? getNotificationListQueryKey().queryFn : skipToken,
     staleTime: 1000 * 60 * 5, // 5분
     gcTime: 1000 * 60 * 1, // 1분
   });
