7c05d7869a61778666cb191bfd543d94f2780815
minseong <kwan0324@naver.com>
2024-10-21 04:11:46 +0900

    Style: add images for home banner

--- apps/web/src/components/home/CarouselWithBanners/index.tsx
+++ apps/web/src/components/home/CarouselWithBanners/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Banner, Carousel } from '@recipic-packages/ui';
-import MockBannerImage from '@/assets/images/mockBanner.webp';
 import { TBannerProps } from '@recipic-packages/ui/components/ui/banner';
+import MainBanner1Image from '@/assets/images/mainBanner1.webp';
+import MainBanner2Image from '@/assets/images/mainBanner2.webp';
+import MainBanner3Image from '@/assets/images/mainBanner3.webp';
 
 type TBannerData = {
   bannerId: number;
@@ -10,7 +12,7 @@ type TBannerData = {
 const banners: TBannerData[] = [
   {
     bannerId: 1,
-    imageUrl: MockBannerImage,
+    imageUrl: MainBanner1Image,
     onClick: () => console.log('Banner 2 clicked'),
     gradientColor: 'black',
     title: '유튜버가 인정한 \n 요아정 꿀조합',
@@ -18,7 +20,7 @@ const banners: TBannerData[] = [
   },
   {
     bannerId: 2,
-    imageUrl: MockBannerImage,
+    imageUrl: MainBanner2Image,
     onClick: () => console.log('Banner 1 clicked'),
     gradientColor: 'primary',
     title: '꿀조합 게시글 작성 \n 이벤트 참여하기',
@@ -26,7 +28,7 @@ const banners: TBannerData[] = [
   },
   {
     bannerId: 3,
-    imageUrl: MockBannerImage,
+    imageUrl: MainBanner3Image,
     onClick: () => console.log('Banner 3 clicked'),
     gradientColor: 'gray',
     title: '설문조사에 참여하기',
