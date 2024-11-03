884071657923143110e946b69d1164f92df5a78c
minseong <kwan0324@naver.com>
2024-11-04 00:44:01 +0900

    Delete: delete copyright menu

--- apps/web/src/components/settings/SettingsRouteMenu/TermsSettingsMenuList/index.tsx
+++ apps/web/src/components/settings/SettingsRouteMenu/TermsSettingsMenuList/index.tsx
@@ -5,7 +5,6 @@ import { TSettingsRouteMenuItem } from '@/types/settings';
 const termsSettings: TSettingsRouteMenuItem[] = [
   { title: '서비스 이용 약관', route: '/settings/terms' },
   { title: '개인정보 처리방침', route: '/settings/privacy' },
-  { title: '저작권', route: '/settings/copyright' },
 ];
 
 export function TermsSettingsMenuList() {
